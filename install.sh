085bdb98245dd52972a4cf859695e74a271fe89f
Lars Sandberg <larslsa@stud.ntnu.no>
2022-07-23 11:32:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 sudo apt install python3-pip
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 sudo apt install htop
+sudo apt install tmux
 sudo apt-get install neofetch
