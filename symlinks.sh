3beea71cac2cf44aea0c53bddebf7c503a072416
Lars Sandberg <larslsa@stud.ntnu.no>
2022-07-23 11:31:53 +0200

    Update symlinks.sh

--- symlinks.sh
+++ symlinks.sh
@@ -3,3 +3,4 @@ ln -sf ~/wsl-setup-dotfiles/.bashrc ~/.bashrc
 ln -sf ~/wsl-setup-dotfiles/.bash_aliases ~/.bash_aliases
 ln -sf ~/wsl-setup-dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/wsl-setup-dotfiles/.hushlogin ~/.hushlogin
+ln -sf ~/wsl-setup-dotfiles/.tmux.conf ~/.tmux.conf
